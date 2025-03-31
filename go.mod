c913eeee0add902e30c047213b3d7f6325e6e190
Jonathon Chew <>
2025-03-31 08:44:07 +0100

    Updating...

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jonathon-chew/Pokedex
+
+go 1.24.1
---
+++ repl_test.go
@@ -0,0 +1,31 @@
+package main_test
+
+import "testing"
+
+cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		// add more cases here
+	}
+
+func TestCleanInput(t *testing.T) {
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		// Check the length of the actual slice against the expected slice
+		// if they don't match, use t.Errorf to print an error message
+		// and fail the test
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			// Check each word in the slice
+			// if they don't match, use t.Errorf to print an error message
+			// and fail the test
+		}
+	}
+}
